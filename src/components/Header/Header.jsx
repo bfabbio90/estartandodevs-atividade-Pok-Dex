52f0e114455e08c8bc7efc6b1c72f135315a4b59
Bruno Fabbio <brunobiodev@gmail.com>
2025-10-14 21:49:56 -0300

    feat: adiciona primeira estrutura principal

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <Link to="/" className="logo">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+            alt="Pokédex Logo"
+          />
+        </Link>
+
+        <nav className="nav">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">Sobre</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
